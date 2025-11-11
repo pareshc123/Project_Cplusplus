ddb8a2b050cb73f1beb190ea50cc26f7748bd575
Paresh Choudhary <pareshc123@gmail.com>
2025-11-11 10:58:50 +0100

    --/PaCh: Rename File source.cpp to introduction.cpp

---
+++ FirstProject/main.cpp
@@ -0,0 +1,13 @@
+/*
+	Welcome to First Program in C++ !!
+*/
+
+#include <iostream> // Include the iostream library
+
+int main() {
+
+	std::cout << "Hello, World!" << std::endl; // Print Hello, World! to the console
+	std::cout << "Welcome to C++ Programming." << std::endl; // Print Welcome to C++ Programming to the console
+
+	return 0; // Return 0 to indicate successful completion
+}
