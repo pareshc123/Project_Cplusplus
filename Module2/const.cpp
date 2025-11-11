62bbf62cb766f6d39491cb538d3b6b28195471c1
Paresh Choudhary <pareshc123@gmail.com>
2025-11-11 16:25:17 +0100

    Const/Module2/PaCh: Const with ref and pointers

--- Module2/const.cpp
+++ Module2/const.cpp
@@ -1,7 +1,29 @@
 /*
 	Constants in C++
+	- Constant variables are variables whose values cannot be changed after they are initialized.
+	- Basic syntax: const data_type VARIABLE_NAME = value;
 
-	syntax: const data_type VARIABLE_NAME = value;
+	Const Qualifier with Pointers:
+	1. Pointer to a constant value:
+		The value being pointed to cannot be changed through the pointer, but pointer can point to another variable
+		Syntax: const int* ptr = &variable;
+
+	2. Constant pointer:
+		The pointer itself cannot point to another variable, but the value being pointed to can be changed.
+		Syntax: int* const ptr = &variable;
+
+	3. Constant pointer to a constant value:
+		Pointer that is both constant and points to a constant value
+		Syntax: const int* const ptr = &variable;
+
+	4. Constant with References:
+		References in C++ can also be made constant, meaning the reference cannot be changed to refer to another variable.
+		Syntax: const int& ref = variable;
+
+	Reading Pointer Declarations “Right to Left”:
+	const int* ptr;    // ptr is a pointer to a constant int
+	int* const ptr2;   // ptr2 is a constant pointer to int
+	const int* const ptr3; // ptr3 is a constant pointer to a constant int
 
 */
 
@@ -17,6 +39,35 @@ int main() {
 	
 	// DAYS_IN_WEEK = 8; // Error: cannot modify a constant variable
 
+	// 1. Pointer to a constant value:
+	int value1 = 10;
+	const int* ptrToConst = &value1;
+	cout << "\nValue pointed to by ptrToConst: " << *ptrToConst << endl;
+	// *ptrToConst = 20; // Error: cannot modify the value being pointed to
+
+	// 2. Constant pointer:
+	int value2 = 30;
+	int* const constPtr = &value2;
+
+	cout << "\nValue pointed to by constPtr: " << *constPtr << endl;
+	*constPtr = 40; // Allowed: modifying the value being pointed to
+	cout << "New value pointed to by constPtr: " << *constPtr << endl;
+	// constPtr = &value1; // Error: cannot change the address stored in a constant pointer
+
+	// 3. Constant pointer to a constant value:
+	int value3 = 50;
+	const int* const constPtrToConst = &value3;
+	cout << "\nValue pointed to by constPtrToConst: " << *constPtrToConst << endl;
+	// *constPtrToConst = 60; // Error: cannot modify the value being pointed to
+	// constPtrToConst = &value1; // Error: cannot change the address stored in a constant pointer
+
+	// 4. Constant with References:
+	int value4 = 70;
+	const int& constRef = value4;
+	cout << "\nValue referred to by constRef: " << constRef << endl;
+	// constRef = 80; // Error: cannot modify the value through a constant reference
+	
+	// End of Program
 	return 0;
 
 }
