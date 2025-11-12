55232188eee37e17d444b7fd6bd3beb4d34afc7b
Paresh Choudhary <pareshc123@gmail.com>
2025-11-12 09:41:08 +0100

    Module2/PaCh: Add Functions inline example

--- Module2/functions.cpp
+++ Module2/functions.cpp
@@ -2,6 +2,10 @@
 	Basic of functions in C++
 	Includes seperate header file for function declarations --> math.h
 	Includes seperate source file for function definitions --> math.cpp
+
+	Note: 
+		This is the main file that uses the functions declared in math.h and defined in math.cpp
+		An inline function must be defined in the header file itself for performance optimization.
 */
 
 // include necessary libraries
@@ -27,6 +31,9 @@ int main() {
 	// call the subtract function
 	subtraction = subtract(num1, num2);
 	cout << "The subtraction is: " << subtraction << endl;
+
+	// call the Square function
+	cout << "The square of " << num1 << " is: " << Square(num1) << endl;
 
 	// End of the program
 	return 0;
--- Module2/math.h
+++ Module2/math.h
@@ -5,3 +5,8 @@ int add(int a, int b);
 
 // function prototype for subtraction
 int subtract(int a, int b);
+
+// function to calculate the square of an integer
+inline int Square(int x) {
+	return x * x;
+}
