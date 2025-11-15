20cfe4f012ac7453b70c80104c0c792ebcc00c12
Paresh Choudhary <pareshc123@gmail.com>
2025-11-15 19:29:59 +0100

    OOPs/PaCh: Add BankAccount.cpp and .h file for BankAccount project

---
+++ Module4-Object-Oriented-Programming/BankAccount.cpp
@@ -0,0 +1,60 @@
+#include "BankAccount.h"
+
+// Default constructor
+BankAccount::BankAccount() {
+
+	accountHolder = "Unknown";
+	accountNumber = 0;
+	balance = 0.0;
+	cout << "\nDefalut Constructor called for Account: " << accountHolder << endl;
+}
+
+// Parameterized constructor
+BankAccount::BankAccount(const string& accHolder, int accNumber, double initialBalance) {
+
+	accountHolder = accHolder;
+	accountNumber = accNumber;
+	balance = initialBalance;
+	cout << "\nParameterized constructor called for account: " << accountHolder << endl;
+}
+
+// Destructor
+BankAccount::~BankAccount() {
+	cout << "\nDestructor was called for account: " << accountHolder << endl;
+}
+
+//Member functions
+
+// Deposit function
+void BankAccount::Deposit(double amount) {
+	balance += amount;
+	cout << "\nHello " << accountHolder << "New Transaction alert !!" << endl;
+	cout << amount << " credited to your account. New Balance: " << balance << endl;
+}
+
+// Withdraw function
+void BankAccount::Withdraw(double amount) {
+
+	cout << "\nNew Withdraw Request initiated for account holder: " << accountHolder << endl;
+	
+	if (balance < amount) {
+		cout << "Request not possible. Insufficient Balance !! Please request a lower amount." << endl;
+		cout << "Current balance: " << balance << endl;
+		return ;
+	}
+	else {
+		balance -= amount;
+		cout << amount << " debited from users account. New Balance: " << balance << endl;
+	}
+	
+}
+
+// Display method
+void BankAccount::DisplayBalance() const {
+
+	cout << "Account Holder: " << accountHolder << endl;
+	cout << "Account Number: " << accountNumber << endl;
+	cout << "Balance: " << balance << endl;
+
+}
+
---
+++ Module4-Object-Oriented-Programming/BankAccount.h
@@ -0,0 +1,34 @@
+#ifndef BANKACCOUNT_H
+#define BANKACCOUNT_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class BankAccount {
+
+private:
+	string accountHolder;
+	int accountNumber;
+	double balance;
+
+public:
+
+	// defualt Constructor
+	BankAccount();
+
+	// Parameterized  Constructor
+	BankAccount(const string& accHolder, int accNumber, double initialBalance);
+
+	// Destructor
+	~BankAccount();
+
+	// Member
+	void Deposit(double amount);
+	void Withdraw(double amount);
+	void DisplayBalance() const;
+
+};
+
+#endif
