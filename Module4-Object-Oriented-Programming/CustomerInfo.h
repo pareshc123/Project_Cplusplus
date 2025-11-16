fa09cc316f3830d76dc5e19b36d181cda0973fde
Paresh Choudhary <pareshc123@gmail.com>
2025-11-16 10:05:00 +0100

    OOps/PaCh: Add new file CustomerInfo.h - introducing Struct

---
+++ Module4-Object-Oriented-Programming/CustomerInfo.h
@@ -0,0 +1,16 @@
+#ifndef CUSTOMERINFO_H
+#define CUSTOMERINFO_H
+
+#include <string>
+
+using namespace std;
+
+struct CustomerInfo
+{
+	string name;
+	string address;
+	string phonenumber;
+
+};
+
+#endif
