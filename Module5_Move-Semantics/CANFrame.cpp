6080f3ad9c4429e7f791f1b4e8cb87ce1494d51f
Paresh Choudhary <pareshc123@gmail.com>
2025-11-17 21:21:45 +0100

    M6/PaCh: Add the CANFrame.h and .cpp template

--- Module5_Move-Semantics/CANFrame.cpp
+++ Module5_Move-Semantics/CANFrame.cpp
@@ -1,11 +1,44 @@
+#include "CANFrame.h"
+
 /*
-	Each CAN frame object will contain:
-
-		CAN ID
-		DLC (data length code)
-		8-byte data field
-		Maybe timestamp later
-
-	We will generate CANFrame objects (either manually or by a generator function) in mainOperatorOverloading.cpp file
-	
+	This .cpp file contains the definitions (/Prototype) of CANFrame Funcitons
 */
+
+CANFrame::CANFrame()
+{
+}
+
+CANFrame::CANFrame(uint32_t id, uint8_t dlc, array<uint8_t, 8>& data)
+{
+}
+
+CANFrame::~CANFrame()
+{
+}
+
+uint32_t CANFrame::getID() const
+{
+	return 0;
+}
+
+uint8_t CANFrame::getDLC() const
+{
+	return 0;
+}
+
+const array<uint8_t, 8>& CANFrame::getdata() const
+{
+	// TODO: insert return statement here
+}
+
+void CANFrame::setID(uint32_t id)
+{
+}
+
+void CANFrame::setDLC(uint32_t dlc)
+{
+}
+
+void CANFrame::setDATA(const array<uint8_t, 8>& data)
+{
+}
---
+++ Module5_Move-Semantics/CANFrame.h
@@ -0,0 +1,60 @@
+#ifndef CANFRAME_H
+#define CANFRAME_H
+
+/*
+	
+	This is a C++ header file (.h) that defines a class called CANFrame.
+
+	A CAN frame is a message sent on a CAN Bus (used in cars, ECUs, sensors, etc.).
+	This class simply stores:
+		- an ID (message identifier)
+		- a DLC (data length = how many bytes)
+		- an 8-byte data array
+
+	This is a container for sending or receiving CAN messages.
+
+	#include <cstdint>
+	This header provides fixed-size integer types, like:
+		uint8_t = unsigned 8-bit integer (0 to 255)
+		uint32_t = unsigned 32-bit integer (0 to 4,294,967,295)
+	These are very common in embedded / automotive, because you want exact size.
+
+*/
+
+#include <array>
+#include <string>
+#include <cstdint>
+
+using namespace std;
+
+class CANFrame
+{
+
+private:
+
+	uint32_t m_id;						// CAN Identifier	
+	uint8_t m_dlc;                     // DLC (data length code) 0-8bytes
+	array<uint8_t, 8> m_data{};		  // 8-byte payload
+
+public:
+
+	// ---- Constructors ----
+	CANFrame();
+	CANFrame(uint32_t id, uint8_t dlc, array<uint8_t, 8>& data);
+
+	// ---- Getters ---- 
+	uint32_t getID() const;
+	uint8_t getDLC() const;
+	const array<uint8_t,8>& getdata() const;
+
+	// ---- Setters ---- 
+	void setID(uint32_t id);
+	void setDLC(uint32_t dlc);
+	void setDATA(const array<uint8_t,8>& data);
+
+	// ---- Destructors ---- 
+	~CANFrame();
+
+};
+
+#endif
