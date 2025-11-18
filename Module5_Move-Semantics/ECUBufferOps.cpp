c73e9f159a063a594bc5cbe1d159764a85761525
Paresh Choudhary <pareshc123@gmail.com>
2025-11-18 09:26:22 +0100

    M6/PaCh: Implement principle of operator Overloading for +=, [], << in ECUDataFrame

--- Module5_Move-Semantics/ECUBufferOps.cpp
+++ Module5_Move-Semantics/ECUBufferOps.cpp
@@ -1,3 +1,39 @@
 /*
 	Contains definitions for operator overloading CAN Frames
 */
+
+#include "ECUDataBuffer.h"
+
+ECUDataBuffer& ECUDataBuffer::operator+=(const CANFrame& frame) {
+
+	// Add Frame to the list
+	CANbufferList.push_back(frame);
+
+	return *this;		// so you can chain: ecu += f1 += f2;
+}
+
+const CANFrame& ECUDataBuffer::operator[](size_t size) const {
+
+	if (size >= CANbufferList.size()) {
+		throw std::out_of_range("ECUDataBuffer: index out of range");
+	}
+
+	// Frames are stored in CANBufferList
+	return CANbufferList[size];
+}
+
+std::ostream& operator<<(std::ostream& os, const ECUDataBuffer& buffer)
+{
+	os << "ECU Data Buffer (" << buffer.CANbufferList.size() << " frames):\n";
+
+	for (size_t i = 0; i < buffer.CANbufferList.size(); ++i)
+	{
+		os << "  [" << i << "] "
+			<< buffer[i].toString() << "\n";   // Using operator[] overloading instead of buffer.CANbufferList[i].tostring()
+	}
+
+	return os;
+}
+
+
+
--- Module5_Move-Semantics/ECUDataBuffer.h
+++ Module5_Move-Semantics/ECUDataBuffer.h
@@ -2,12 +2,17 @@
 #define ECUDATABUFFER_H
 
 #include <cstddef>  // for size_t
+#include "CANFrame.h"
+#include <vector>
 
 class ECUDataBuffer {
 
 private:
 	int* dataPtr;		// pointer to dynamically allocated CAN data buffer
 	size_t size;    // number of CAN data elements to be stored
+
+	// The ECU stores a list of incoming CAN frames in a container
+	std::vector<CANFrame> CANbufferList;
 
 public:
 
@@ -25,8 +30,13 @@ public:
 	int GetPtrValue() const;
 	void SetPtrValue(size_t s);
 
+	// Operator Overloading function declaration
+	ECUDataBuffer& operator+=(const CANFrame& frame);       // Add a CAN frame to ECU buffer
+	const CANFrame& operator[](size_t index) const;			// Access stored CAN frames
+	friend std::ostream& operator<<(std::ostream& os, const ECUDataBuffer& buffer);   // << (friend) to print the whole buffer
+	
 	// Destructor
-	~ECUDataBuffer();  
+	~ECUDataBuffer();
 };
 
 #endif
