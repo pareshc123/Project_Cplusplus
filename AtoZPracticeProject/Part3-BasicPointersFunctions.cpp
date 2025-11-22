b350f476f148da7da00247a962a472fae553e0de
Paresh Choudhary <pareshc123@gmail.com>
2025-11-22 18:21:09 +0100

    --/PaCh: Completed the BasicPointerFunctions.cpp file. Moving to advanced

--- AtoZPracticeProject/Part3-BasicPointersFunctions.cpp
+++ AtoZPracticeProject/Part3-BasicPointersFunctions.cpp
@@ -92,6 +92,15 @@ using namespace std;
 int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
+int divi(int a, int b);
+
+// Function pointer passed as an argument
+int executeOperation(int a, int b, int (* const operationPtr)(int, int));
+// const ensures the callback cannot be reassigned inside the function.
+
+
+// Functions that returns a Function pointers
+int (*getOperator(char op))(int, int);
 
 int main() {
 
@@ -114,6 +123,47 @@ int main() {
     cout << "  Calling the function via Pointer: " << endl;
     cout << "  The output of Multiplication(10, 5) is: " << basicPtr(10, 5) << endl;
 
+	//  ------  Exercise2: Pass Function Pointer to Another Function:  -------
+	cout << "\nExercise2: Pass Function Pointer to Another Function:" << endl;
+	cout << "Result for addition: " << executeOperation(20, 8, add) << endl;
+	cout << "Result for subtraction: " << executeOperation(20, 8, sub) << endl;
+	cout << "Result for multiplication: " << executeOperation(20, 8, mul) << endl;
+
+	//  ------  Exercise3: Array of Function Pointer:  -------
+	cout << "\nExercise3: Array of Function Pointer:" << endl;
+	int (*arrayFuncPtr[4])(int, int) = {add, sub, mul, divi};
+	
+	int userChoice;
+	cout << "Please Enter the choice of arithmetic operation required: " << endl;
+	cout << "  0 = Addition, 1 = Subtraction, 2 = Multiply, 3 = Division" << endl;
+	cin >> userChoice;
+	
+	if (userChoice >= 4) {
+		cout << "User Choice Incorrect. Please try again." << endl;
+		return 0;
+	}
+
+	int result = arrayFuncPtr[userChoice](10, 5);
+	switch (userChoice) {
+	case 0: cout << "Addition request was initiated. The result for the user input is: " << result << endl; break;
+	case 1: cout << "Subtraction request was initiated. The result for the user input is: " << result << endl; break;
+	case 2: cout << "Multiplication request was initiated. The result for the user input is: " << result << endl; break;
+	case 3: cout << "Division request was initiated. The result for the user input is: " << result << endl; break;
+	}
+
+	//  ------  Exercise4: Functions that returns a Function Pointer:  -------
+	cout << "\nExercise4: Functions that returns a Function Pointer:" << endl;
+	char userOpChoice;
+	cout << "Please Enter the choice of arithmetic operation required: " << endl;
+	cout << "  + = Addition, - = Subtraction, * = Multiply, / = Division" << endl;
+	cin >> userOpChoice;
+
+	// function call
+	auto returnfuncPtr = getOperator(userOpChoice);
+	int resultfuncPtr = returnfuncPtr(10, 5);
+
+	cout << "Result of user Choice of operation '" << userOpChoice << "' is: " << resultfuncPtr << endl;
+
 	// End of Program
 	return 0;
 }
@@ -121,3 +171,30 @@ int main() {
 int add(int a, int b) { return a + b; }
 int sub(int a, int b) { return a - b; }
 int mul(int a, int b) { return a * b; }
+int divi(int a, int b) {
+
+	if (b == 0) {
+		cout << "Division not possible, denominator is 0" << endl;
+		return -1;
+	}
+	else {
+		return a / b;
+	}
+
+}
+
+int (*getOperator(char op))(int, int) {
+
+	switch (op) {
+	case '+': cout << "Addition operation was requested." << endl; return add;
+	case '-': cout << "Subtraction operation was requested." << endl; return sub;
+	case '*': cout << "Multiplication operation was requested." << endl; return mul;
+	case '/': cout << "Division operation was requested." << endl; return divi;
+	default: cout << "Invalid input, returning add()\n" << endl;  return add;
+	}
+}
+
+int executeOperation(int a, int b, int (* const operationPtr)(int, int)) {
+
+	return operationPtr(a, b);
+}
