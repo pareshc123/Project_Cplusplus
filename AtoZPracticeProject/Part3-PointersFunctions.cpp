f441527852fff85e6dac82aaf26dd93d533ce49f
Paresh Choudhary <pareshc123@gmail.com>
2025-11-22 10:56:31 +0100

    --/PaCh: Adding function pointer to Part5

--- AtoZPracticeProject/Part3-PointersFunctions.cpp
+++ AtoZPracticeProject/Part3-PointersFunctions.cpp
@@ -32,13 +32,22 @@
 		int result = funcPtr(10, 20);
 
 	2. Passing Function Pointers to Other Functions. why ?? ==> To allow a function to run a callback.
+		Note: The function (in below example: execute()) that would receive a function pointer(*operation with two int arguments) 
+			  as a argument can call back the function (execute(add)) that this pointer will point to.
 		
-		void execute(int (*operation)(int, int)) {
-			int result = operation(10, 5);
+		void execute(int (*operation)(int, int)) {   // function pointer as an argument
+			int result = operation(10, 5);          // call-back function, since pointer (*operation) is pointing toward add()
 		}
 
-		execute(add);  // call it;
+		int main(){
+			
+			int (*Ptr)(int, int) = add;
+			execute(add);  // call it;
 
+				OR
+
+			execute(add);  because name of the function itself return the address.
+		}
 		This is how:
 			hardware callbacks
 			interrupt handlers
@@ -74,3 +83,41 @@
 		int result = operations[2](5, 3);  // calls mul(5,3)
 
 */
+
+#include <iostream>
+
+using namespace std;
+
+// Function prototype (declaration)
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+
+int main() {
+
+	//  ------  Exercise1: Basic Function pointer:  -------
+	cout << "\nExercise1: Basic Function Pointer:" << endl;
+	int (*basicPtr)(int, int);
+
+	cout << "\nAssigning the Addition function to basicPtr: " << endl;
+	basicPtr = add;
+	cout << "  Calling the function via Pointer : " << endl;
+	cout << "  The output of Addition(10, 5) is: " << basicPtr(10, 5) << endl;
+
+	cout << "\nAssigning the Subtraction function to basicPtr: " << endl;
+    basicPtr = &sub;
+    cout << "  Calling the function via Pointer: " << endl;
+    cout << "  The output of Subtraction(10, 5) is: " << basicPtr(10, 5) << endl;
+    
+    cout << "\nAssigning the Multpily function to basicPtr: " << endl;
+    basicPtr = mul;
+    cout << "  Calling the function via Pointer: " << endl;
+    cout << "  The output of Multiplication(10, 5) is: " << basicPtr(10, 5) << endl;
+
+	// End of Program
+	return 0;
+}
+
+int add(int a, int b) { return a + b; }
+int sub(int a, int b) { return a - b; }
+int mul(int a, int b) { return a * b; }
