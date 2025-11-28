a55789cf0773fe7b0ee3ffc16cc201a680e010d6
Paresh Choudhary <pareshc123@gmail.com>
2025-11-28 22:13:42 +0100

    M6/PaCh: Two basic smart pointer exercises completed

--- Module3_Memory-Management/BasicsSmartPointers.cpp
+++ Module3_Memory-Management/BasicsSmartPointers.cpp
@@ -51,3 +51,44 @@
 		3. weak_ptr: std::weak_ptr<Car> wPtr = cPtr (always created from shared_ptr)
 
 */
+
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+struct Motor {
+
+	int rpm = 0;
+
+	Motor() {};
+	Motor(int speed) : rpm(speed) {};
+	~Motor() {};
+};
+
+int main() {
+
+	// Exercise1: Beginner Level - owning a simple Integer
+	cout << "\nExercise1: Beginner Level - owning a simple Integer:" << endl;
+	auto value = make_unique<int>(42);
+	cout << "Value = " << *value << endl;
+
+	// Exercise2: Owning a simple object
+	cout << "\nExercise2: Owning a simple object:" << endl;
+	auto rpmPtr1 = make_unique<Motor>(5000);
+	cout << "The speed of Motor at start is: " << rpmPtr1->rpm << endl;
+	
+	auto rpmPtr2 = make_unique<Motor>();
+	rpmPtr2->rpm = 1500;
+	cout << "The speed of Motor after few seconds is: " << rpmPtr2->rpm << endl;
+
+	// Exercise3: unique_ptr automatically deleting
+	cout << "\nExercise3: unique_ptr automatically deleting:" << endl;
+
+
+	// Exercise4: Moving unique_ptr
+	cout << "\nExercise4: Moving unique_ptr:" << endl;
+
+
+	return 0;
+}
