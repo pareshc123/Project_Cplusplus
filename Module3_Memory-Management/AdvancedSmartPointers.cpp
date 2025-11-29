1788fbd92bed34be82340e6326dcd1e15a9833e2
Paresh Choudhary <pareshc123@gmail.com>
2025-11-29 22:32:38 +0100

    M6/PaCh: Add new file AdvancedSmartPointers.cpp

---
+++ Module3_Memory-Management/AdvancedSmartPointers.cpp
@@ -0,0 +1,10 @@
+/*
+    ADVANCED AUTOMOTIVE SMART POINTER EXERCISES
+    --------------------------------------------
+    Exercises Covered:
+      9. unique_ptr + ECU factory
+      10. shared_ptr CAN decoder shared by modules
+      11. weak_ptr for breaking cyclic ECU references
+      12. unique_ptr[] sensor buffer
+
+*/
