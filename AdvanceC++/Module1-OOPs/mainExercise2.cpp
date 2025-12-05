4f36ae667a87df5a1d5820488398c78d408d088b
Paresh Choudhary <pareshc123@gmail.com>
2025-12-05 21:03:25 +0100

    --/PaCh: clean the mainExercise2.cpp file

--- AdvanceC++/Module1-OOPs/mainExercise2.cpp
+++ AdvanceC++/Module1-OOPs/mainExercise2.cpp
@@ -5,34 +5,34 @@ int main() {
     
     std::cout << "\n=== START OF EXERCISE 2 ===\n";
 
+    std::cout << "Invoke Class function using Vehicle Class: " << std::endl;
     Vehicle veh;
-
     veh.brand = "BMW";
     veh.year = 2020;
-
     veh.setEngineInfo("V8", "Diesel", 200);
-
     veh.displayVehicleInfo();
-
+    
     std::cout << std::endl;
 
+    std::cout << "Invoke Class function using CAR Class: " << std::endl;
     Car c;
     c.brand = "Porsche";
     c.year = 2021;
-
     c.setEngineInfo("V4", "Gasoline", 120);
-
     c.displayVehicleInfo();
 
     std::cout << std::endl;
 
-    Vehicle* vehPtr = new Car;
+    std::cout << "Invoke Class function using CAR Class via pointer: " << std::endl;
+    Vehicle* vehPtr = new Car(" ", 0, 0);       // random information about car
     vehPtr->brand = "Mercedes";
     vehPtr->year = 2021;
-
     vehPtr->setEngineInfo("V12", "Hydrogen", 256);
-
     vehPtr->displayVehicleInfo();
+    delete vehPtr;
+
+    std::cout << "Invoke Class function using CAR Class via pointer Array: " << std::endl;
+
 
     std::cout << "\n=== END OF EXERCISE 2 ===\n";
 
