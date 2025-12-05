549fa6e9142aa1d44da3703efe82a606392d2b11
Paresh Choudhary <pareshc123@gmail.com>
2025-12-05 20:51:36 +0100

    --/PaCh: Call the Car class with class object and pointer

--- AdvanceC++/Module1-OOPs/mainExercise2.cpp
+++ AdvanceC++/Module1-OOPs/mainExercise2.cpp
@@ -1,4 +1,5 @@
 #include "Vehicle.h"
+#include "Car.h"
 
 int main() {
     
@@ -12,6 +13,26 @@ int main() {
     veh.setEngineInfo("V8", "Diesel", 200);
 
     veh.displayVehicleInfo();
+
+    std::cout << std::endl;
+
+    Car c;
+    c.brand = "Porsche";
+    c.year = 2021;
+
+    c.setEngineInfo("V4", "Gasoline", 120);
+
+    c.displayVehicleInfo();
+
+    std::cout << std::endl;
+
+    Vehicle* vehPtr = new Car;
+    vehPtr->brand = "Mercedes";
+    vehPtr->year = 2021;
+
+    vehPtr->setEngineInfo("V12", "Hydrogen", 256);
+
+    vehPtr->displayVehicleInfo();
 
     std::cout << "\n=== END OF EXERCISE 2 ===\n";
 
