fc312aedd329b18a576f415eb19a1713d9b04dde
Paresh Choudhary <pareshc123@gmail.com>
2025-12-07 13:50:14 +0100

    --/PaCh: Implement .cpp and .h file

--- AdvanceC++/Module1-OOPs/Motorcycle.h
+++ AdvanceC++/Module1-OOPs/Motorcycle.h
@@ -1,7 +1,37 @@
 #pragma once
 #include "Vehicle.h"
+
+// Enum representing motorcycle types
+enum BikeType {
+    Sport,
+    Cruiser,
+    Touring,
+    Dirt,
+    Quad,
+    Scooter,
+    NotDefined
+};
+
 class Motorcycle :
     public Vehicle
 {
+
+protected:
+    BikeType bikeType;
+
+public:
+
+    Motorcycle();
+    Motorcycle(const std::string& brand, int yr, BikeType type);
+    Motorcycle(const std::string& brand, int yr, const Engine& e, BikeType type);
+
+    // Display motorcycle-specific info
+    void displayVehicleInfo() const override;
+
+    // Override abstract function
+    std::string getType() const override;
+
+    ~Motorcycle();
+
 };
 
