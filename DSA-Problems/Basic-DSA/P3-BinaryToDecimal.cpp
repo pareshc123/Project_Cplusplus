d8261a27ff4a17a997fe856e8ccccf5284290446
Paresh Choudhary <pareshc123@gmail.com>
2026-02-01 18:57:36 +0100

    DSA/PaCh: P3-Add descritption

--- DSA-Problems/Basic-DSA/P3-BinaryToDecimal.cpp
+++ DSA-Problems/Basic-DSA/P3-BinaryToDecimal.cpp
@@ -1,3 +1,30 @@
+/*
+
+	Given a string b representing a Binary Number, The problem is to find its decimal equivalent.
+
+	Examples:
+
+	Input : b = 111
+		Output : 7
+		Explanation : The decimal equivalent of the binary number 111 is 22 + 21 + 20 = 7.
+	
+	Input : b = 1010
+		Output : 10
+		Explanation : The decimal equivalent of the binary number 1010 is 23 + 21 = 10.
+	
+	Input: b = 100001
+		Output: 33
+		Explanation : The decimal equivalent of the binary number 100001 is 25 + 20 = 33.
+	
+	Constraints:
+		1 <= number of bits in binary number  <= 16
+
+	Expected Complexities
+		Time Complexity: O(log n)  
+		Auxiliary Space: O(1)
+
+*/
+
 #include <iostream>
 #include <string>
 
