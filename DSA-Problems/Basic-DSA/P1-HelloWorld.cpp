96dea23deb046982d78c1d9fe17f93d08973b52d
Paresh Choudhary <pareshc123@gmail.com>
2026-02-01 12:07:11 +0100

    DSA/PaCh: Basic Problem 1 - HelloWorld

---
+++ DSA-Problems/Basic-DSA/P1-HelloWorld.cpp
@@ -0,0 +1,27 @@
+/*
+
+	Example 1:
+
+	Input:
+		No Input
+
+	Output:
+		Hello World
+
+	Expected Time Complexity: O(1)
+	Expected Auxiliary Space: O(1)
+
+	Your Task:
+	In the function helloWorld(), output a line "Hello World" (without quotes).
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	std::cout << "Hello World";
+
+}
