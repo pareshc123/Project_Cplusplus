a5250b3fc1cf9019d92c86d4e3feca29cddda6a1
Paresh Choudhary <pareshc123@gmail.com>
2026-02-01 15:34:55 +0100

    DSA/PaCh: basic problem 3 - BinaryToDecimal - Optimize code

--- DSA-Problems/Basic-DSA/P3-BinaryToDecimal.cpp
+++ DSA-Problems/Basic-DSA/P3-BinaryToDecimal.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <string>
 
+void optimize_code(std::string& str) {
+
+	std::cout << "*** Optimize code ***" << std::endl;
+
+	int dec = 0;
+	for (char c : str) {
+
+		dec = dec * 2 + (c - '0');
+	}
+
+	std::cout << "dec: " << dec << std::endl;
+
+}
+
 int main() {
 
 	std::string str = "1011";
+
+	std::cout << "*** Method one ***" << std::endl;
 
 	int exponent = 1 << (str.length() - 1);   //  Left Bit shifting (powers of 2 only)
 	int dec = 0;
@@ -15,7 +31,9 @@ int main() {
 		exponent >>= 1;    // Right Bit shifting ( exponent = exponent >> 1)
 	}
 
-	std::cout << "dec: " << dec << std::endl;
+	std::cout << "dec: " << dec << "\n" <<std::endl;
+
+	optimize_code(str);
 
 	return 0;
 }
