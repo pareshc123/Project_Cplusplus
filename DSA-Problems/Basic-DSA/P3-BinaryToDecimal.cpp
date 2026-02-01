12a24ede1b3ac06b5477ee1fec78653e2be7bc0e
Paresh Choudhary <pareshc123@gmail.com>
2026-02-01 15:05:12 +0100

    DSA/PaCh: basic problem 3 - BinaryToDecimal - method 1

---
+++ DSA-Problems/Basic-DSA/P3-BinaryToDecimal.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+	std::string str = "1011";
+
+	int exponent = 1 << (str.length() - 1);   //  Left Bit shifting (powers of 2 only)
+	int dec = 0;
+	
+	for (int i = 0; i < str.length(); ++i) {
+
+		dec += (str[i] - '0') * exponent;
+
+		exponent >>= 1;    // Right Bit shifting ()
+	}
+
+	std::cout << "dec: " << dec << std::endl;
+
+	return 0;
+}
