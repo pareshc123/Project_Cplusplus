7eef714c6ec8afa155710d38efc3c3a100a0fe34
Paresh Choudhary <pareshc123@gmail.com>
2026-02-05 22:53:56 +0100

    DSA/PaCh: basicProblem - printSquare - optimizationFix-1

--- DSA-Problems/Basic-DSA/P15-PrintSquare.cpp
+++ DSA-Problems/Basic-DSA/P15-PrintSquare.cpp
@@ -7,10 +7,10 @@ void bruteForceCode(int n) {
     for (int row = 0; row < n; ++row) {
         for (int column = 0; column < n; ++column) {
             if (row == 0 || row == n - 1 || column == 0 || column == n - 1) {
-                cout << "*";
+                cout << "* ";
             }
             else {
-                cout << " ";
+                cout << "  ";
 
             }
         }
@@ -20,14 +20,35 @@ void bruteForceCode(int n) {
 
 
 void optimizeCode(int n) {
+    
+    string res = "";
 
+    for (int row = 0; row < n; ++row) {
+        
+        if (row == 0 || row == n - 1) {
+            
+            for (int column = 0; column < n; ++column) {           // first and last rows
+                res += "* ";
+            }
+        } else {
+            res += "* ";
+            for (int column = 0; column < n - 2; ++column) {       // middle rows:
+                res += "  ";
+            }
+            res += "* ";
+        }
+        res += "\n";
+
+    }
+
+    cout << res;
 }
 
 
 int main() {
 
     // code here
-    int n = 5;
+    int n = 2;
     
     bruteForceCode(n);
     optimizeCode(n);
