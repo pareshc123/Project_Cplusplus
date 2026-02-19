5b219cf56a35e9d08dc336d53b4c5a9e6dea2507
Paresh Choudhary <pareshc123@gmail.com>
2026-02-19 21:38:02 +0100

    --/PaCh: add the new file basicFilesSystem.cpp

---
+++ AdvanceC++/Moduel3-Files_InputOutput/basicsFileSystemLibrary.cpp
@@ -0,0 +1,35 @@
+/*
+	The filesystem library was introduced in C++17 and lives in:
+
+	#include <filesystem>
+	namespace fs = std::filesystem;
+
+	It allows you to:
+
+		- Work with paths
+		- Create/delete directories
+		- Check file existence
+		- Get file sizes
+		- Iterate through folders
+		- Manage logs
+		- Validate file integrity
+
+	In automotive & cybersecurity, this is important for:
+
+		- ECU logging systems
+		- Secure log storage
+		- Firmware update validation
+		- Digital forensics tools
+
+*/
+
+#include<iostream>
+#include<string>
+#include<filesystem>
+
+namespace fs = std::filesystem;
+
+
+int main() {
+	return 0;
+}
