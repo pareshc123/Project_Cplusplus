b20d9afe69c414b9cd92446d1cc4b3e009afb234
Paresh Choudhary <pareshc123@gmail.com>
2026-03-05 21:32:07 +0100

    --/PaCh: Finished Ex 3: NNTA-Non type template argument

--- AdvanceC++/Template/mainTemplates-P1.cpp
+++ AdvanceC++/Template/mainTemplates-P1.cpp
@@ -10,14 +10,14 @@ using  std::cout;
 using  std::endl;
 
 
-// Exercise 1
+// Exercise 1: Primary template
 template<typename T>
 T add(T a, T b) {
 	return a + b;
 }
 
 
-// Exercise 2
+// Exercise 2: Explicit specialization
 template<typename T>
 bool areEqual(T a, T b) {
 	return (a == b) ? true : false;
@@ -27,6 +27,18 @@ template<>
 bool areEqual<const char*>(const char* a, const char* b) {
 	return (strcmp(a, b) == 0) ? true : false;
 }
+
+
+// Exerice 3: Non type Template Argument
+template <typename T, int N>
+void printArray(T(&arr)[N]) {
+	cout << "Size of Array is: " << N << endl;
+	for (int i = 0; i < N; ++i) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
 
 int main() {
 
@@ -38,7 +50,7 @@ int main() {
 	cout << "add(5L, 10L): " << add(5L, 10L) << ", Success !!" << endl;
 	// cout << "add(5, 10L): " << add(5, 10L) << ", Failed -- different argument types !!" << endl;
 
-	cout << "\n================ Exercise2:Explicit Specialization ================" << endl;
+	cout << "\n================ Exercise2: Explicit Specialization ================" << endl;
 	const char* a = "HELLO";
 	const char* b = "HELLO";
 	if (areEqual(a, b)) {
@@ -49,7 +61,14 @@ int main() {
 
 	}
 
+	cout << "\n================ Exercise3: Array Size Deduction ================" << endl;
+	int arr1[4] = { 1,2,3,4 };
+	double arr2[3] = { 1.1,2.2,3.3 };
 
+	printArray(arr1);
+	printArray(arr2);
+
+	cout << "\n================ Exercise4: Compile time buffer ================" << endl;
 
     return 0;
 }
