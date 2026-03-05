ae726cd3177482604c42e6eef00c954b21c7612c
Paresh Choudhary <pareshc123@gmail.com>
2026-03-05 21:25:48 +0100

    --/PaCh: Exercise 2 completed - explicit specilaization

--- AdvanceC++/Template/mainTemplates-P1.cpp
+++ AdvanceC++/Template/mainTemplates-P1.cpp
@@ -17,6 +17,17 @@ T add(T a, T b) {
 }
 
 
+// Exercise 2
+template<typename T>
+bool areEqual(T a, T b) {
+	return (a == b) ? true : false;
+}
+
+template<>
+bool areEqual<const char*>(const char* a, const char* b) {
+	return (strcmp(a, b) == 0) ? true : false;
+}
+
 int main() {
 
 	cout << "================ Exercise1:Template Deduction Deep Dive ================" << endl;
@@ -26,6 +37,19 @@ int main() {
 	cout << "add('A', 'B'): " << add('A', 'B') << ", Success - but wrong results !!" << endl;
 	cout << "add(5L, 10L): " << add(5L, 10L) << ", Success !!" << endl;
 	// cout << "add(5, 10L): " << add(5, 10L) << ", Failed -- different argument types !!" << endl;
+
+	cout << "\n================ Exercise2:Explicit Specialization ================" << endl;
+	const char* a = "HELLO";
+	const char* b = "HELLO";
+	if (areEqual(a, b)) {
+		cout << "Comparison of " << a << ", " << b << " is a Success !!" << endl;
+	}
+	else {
+		cout << "Comparison of " << a << ", " << b << " is Failed !!" << endl;
+
+	}
+
+
 
     return 0;
 }
