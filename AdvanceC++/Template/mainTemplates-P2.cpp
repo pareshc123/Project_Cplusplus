0e9cb1e676e984d8cad78c57d660ae8a0ddf9ea8
Paresh Choudhary <pareshc123@gmail.com>
2026-03-08 15:52:14 +0100

    --/PaCh: Completed Exercise 2 -Serialization of Frame

--- AdvanceC++/Template/mainTemplates-P2.cpp
+++ AdvanceC++/Template/mainTemplates-P2.cpp
@@ -16,16 +16,23 @@
 	------------------------------------------
 		lvalue			forwarded as lvalue
 		rvalue			forwarded as rvalue
+	
+	Perfect forwarding ensures:
+		copy when needed
+		move when possible
 
 */
 
 #include<iostream>
 #include<cstdint>
 #include<array>
+#include<vector>
 
 using std::cout;
 using std::endl;
 
+
+// Exercise 1
 struct CanFrame {
 	uint32_t id;
 	std::array<uint8_t, 8> data;
@@ -45,6 +52,24 @@ public:
 };
 
 
+// Exericse 2
+struct Payload {
+	std::vector<uint8_t> buffer;          // Ethernet payloads can be large, so vector is acceptable
+};
+
+class Seralizer {
+
+public:
+	template<typename T>
+	static Payload serilaize(T&& data) {
+		Payload p;
+		p.buffer = std::forward<T>(data);
+		return p;
+	}
+
+};
+
+
 int main() {
 	
 	cout << "============ Exercise 1 - CAN Message Wrapper ============" << endl;
@@ -58,8 +83,25 @@ int main() {
 		std::array<uint8_t, 8>{1, 2, 3, 4, 5, 6, 7, 8}
 	);
 
-	cout << "============ Exercise 2 - SOME/IP Payload Builder ============" << endl;
 
+	cout << "============ Exercise 2 - SOME/IP Payload Builder ============" << endl;
+	cout << "Case 1 (lvalue): Copy Payload" << endl;
+	Seralizer sData;
+	sData.serilaize(data);
+
+	cout << "Case 2 (rvalue): move payload" << endl;
+	Seralizer sData;
+	sData.serilaize(std::array<uint8_t, 8>{1, 2, 3, 4, 5, 6, 7, 8});
+	
+
+	cout << "============ Exercise 3 - Secure Key Wrapper ============" << endl;
+	cout << "Case 1 (lvalue): Copy Payload" << endl;
+	Seralizer sData;
+	sData.serilaize(data);
+
+	cout << "Case 2 (rvalue): move payload" << endl;
+	Seralizer sData;
+	sData.serilaize(std::array<uint8_t, 8>{1, 2, 3, 4, 5, 6, 7, 8});
 
 	return 0;
 }
