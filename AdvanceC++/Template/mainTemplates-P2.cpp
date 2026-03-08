e7d16d5ab2a45a64f3c9ae30b6c54a784155c8fe
Paresh Choudhary <pareshc123@gmail.com>
2026-03-08 11:57:04 +0100

    --/PaCh: Exercise 1 completed - CAN message wrapper

---
+++ AdvanceC++/Template/mainTemplates-P2.cpp
@@ -0,0 +1,65 @@
+/*
+	
+	Perfect forwarding: 
+		Perfect forwarding preserves the original value category (lvalue/rvalue) when passing arguments to another function.
+
+	we use std::forward
+	template<typename T1, typename T2>
+	Employee(T1&& name, T2&& id)
+		: m_Name{std::forward<T1>(name)},
+		  m_Id{std::forward<T2>(id)}
+	{}
+	
+	Now behaviour becomes:
+
+	Argument type	           Result
+	------------------------------------------
+		lvalue			forwarded as lvalue
+		rvalue			forwarded as rvalue
+
+*/
+
+#include<iostream>
+#include<cstdint>
+#include<array>
+
+using std::cout;
+using std::endl;
+
+struct CanFrame {
+	uint32_t id;
+	std::array<uint8_t, 8> data;
+};
+
+
+class CanMessage {
+
+	CanFrame frame;
+
+public:
+	template<typename T>
+	CanMessage(uint32_t id, T&& payload)
+		: frame{ id, std::forward<T>(payload) }
+	{
+	}
+};
+
+
+int main() {
+	
+	cout << "============ Exercise 1 - CAN Message Wrapper ============" << endl;
+	cout << "Case 1 (lvalue): Copy Payload" << endl;
+	std::array<uint8_t, 8> data = { 1,2,3,4,5,6,7,8 };
+	CanMessage msg(0x123, data);
+
+	cout << "Case 2 (rvalue): move payload" << endl;
+	CanMessage msg(
+		0x123,
+		std::array<uint8_t, 8>{1, 2, 3, 4, 5, 6, 7, 8}
+	);
+
+	cout << "============ Exercise 2 - SOME/IP Payload Builder ============" << endl;
+
+
+	return 0;
+}
