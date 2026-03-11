8c09d53c15ffafa1b70f731bd21a7edc42944f1f
Paresh Choudhary <pareshc123@gmail.com>
2026-03-11 20:58:13 +0100

    MessageStack/PaCh: Add the main file with project description

---
+++ AdvanceC++/Template/main.cpp
@@ -0,0 +1,28 @@
+/*
+	
+	Project: Generic Automotive Message Stack (ECU_Message_Framework)
+
+	Goal: create a type-safe message stack for ECU communication that can store different types of automotive messages.
+			basically: Create a generic ECU message processing framework.
+
+			src/
+			 |--- MessageTypes.hpp
+			 |--- MessageStack.hpp
+			 |--- Logger.hpp
+			 |--- Serializer.hpp
+			 |--- SecureBuffer.hpp
+			 |___ main.cpp
+
+	Think of something used internally in middleware supporting:
+		CAN frames
+		SOME/IP messages
+		Diagnostic messages
+
+	Protocols commonly used in ECUs include:
+		Controller Area Network
+		SOME/IP
+		Unified Diagnostic Services
+
+*/
+
+
