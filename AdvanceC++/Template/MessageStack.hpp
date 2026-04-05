8fd05456be247305b0c92a3478eb561281ad12e8
Paresh Choudhary <pareshc123@gmail.com>
2026-04-05 12:37:29 +0200

    MessageStack/PaCh: Implement embedded constraints

--- AdvanceC++/Template/MessageStack.hpp
+++ AdvanceC++/Template/MessageStack.hpp
@@ -6,7 +6,7 @@ using std::cout;
 template <typename T, size_t SIZE>
 class MessageStack {
 
-	T Buffer[SIZE];
+	T Buffer[SIZE] = {};
 	int idx_pos{ -1 };
 
 public:
@@ -15,18 +15,20 @@ public:
 		cout << "MessageStack: Default constructor Invoked\n";
 	}
 
-	MessageStack(const T&& buff): Buffer(std::forward<T>(buff))  {
-		cout << "Default MessageStack constructor Invoked\n";
-	}
-
 	void push(const T& message) {
+		if (idx_pos >= SIZE) {
+			cout << "Stack Overflow!\n";
+			return;
+		}
 		Buffer[++idx_pos] = message;
 	}
 
 	void pop();
 
 	const T& top() const {
-
+		if (idx_pos < 0) {
+			throw std::out_of_range("Stack is empty");
+		}
 		return Buffer[idx_pos];
 		
 	}
@@ -54,5 +56,9 @@ public:
 
 template <typename T, size_t SIZE>
 void MessageStack<T, SIZE>::pop() {
+	if (idx_pos < 0) {
+		std::cout << "Stack underflow!\n";
+		return;
+	}
 	--idx_pos;
 }
